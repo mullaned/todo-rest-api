48a3fa270522b894a8eeb502ba56d126af76f4b3
David M <mullaned@gmail.com>
2018-12-06 17:53:11 +0000

    update debug status

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.set('debug', true);
-
+// mongoose.set('debug', true);
+// console.log(process.env.MONGO_ATLAS_PW);
 // Connect mongoose
 mongoose.connect('mongodb://dbUser:blabla1@todoapi-shard-00-00-x6rgb.mongodb.net:27017,todoapi-shard-00-01-x6rgb.mongodb.net:27017,todoapi-shard-00-02-x6rgb.mongodb.net:27017/test?ssl=true&replicaSet=todoapi-shard-0&authSource=admin&retryWrites=true',{useMongoClient: true});
 
